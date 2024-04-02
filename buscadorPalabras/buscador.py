21cf7d6a048ea636fa97f75ba2cb3f0ceb744257
Jafet <moranorozcokevinjafet@gmail.com>
2024-04-01 22:25:56 -0600

    scrapping in the wbe

---
+++ buscadorPalabras/buscador.py
@@ -0,0 +1,22 @@
+from automata import Automata
+import get_noticia as getter
+
+def main(random = False):
+  words = ''
+  if not random:
+    words = getter.get_noticia()
+  else:
+    opc = input('1. Introducir la url de la noticia \n2. Seleccionar el archivo de texto test_text.txt \nSeleccione una opción:')
+    if opc == '1':
+      url = input('Introduzca la url de la noticia: ')
+      words = getter.get_noticia(url)
+    else:
+      with open('test_text.txt', 'r') as file:
+        words = file.read()
+  buscador = Automata('tablaDFA.csv')
+  buscador.run(words)
+  print('Se ha creado el archivo registro_estados.txt')
+
+
+if __name__ == "__main__":
+  main()
---
+++ buscadorPalabras/get_noticia.py
@@ -0,0 +1,37 @@
+import requests
+from bs4 import BeautifulSoup
+
+# cambiamos el user-agent para que no nos bloquee la web
+headers = {
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.3'
+}
+
+def get_noticia(url = 'https://elpais.com/mexico/2024-01-16/mexico-mantiene-los-altos-niveles-de-violencia-y-registra-30523-asesinatos-en-2023.html'):
+
+  # Realizamos la petición a la web
+  response = requests.get(url, headers=headers)
+
+  # si no obtenemos una respuesta correcta
+  if response.status_code != 200:
+    print('Error al obtener la noticia')
+    return None
+  
+  soup = BeautifulSoup(response.text, 'lxml')
+
+  # obtenemos las noticias con xpath //div[@data-dtm-region="articulo_cuerpo"]
+  news = soup.find('div', attrs={'data-dtm-region': 'articulo_cuerpo'})
+
+  if news is None:
+    print('Error al obtener la noticia')
+    return None
+  
+  # guardamos el texto de la noticia en un archivo con codificación utf-8
+  with open('noticia.txt', 'w', encoding='utf-8') as file:
+    file.write(news.text)
+
+  return news.text
+
+
+if __name__ == "__main__":
+  text = get_noticia()
+  print(text)
